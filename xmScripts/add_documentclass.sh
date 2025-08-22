26a2cd0caeb93fd5740b1ecd7c7a468e118a83a1
Wim Obbels <wim@obbels.be>
2025-08-22 12:42:29 +0200

    fix some erros (PNG/missing item/counter too large)

--- xmScripts/add_documentclass.sh
+++ xmScripts/add_documentclass.sh
@@ -5,3 +5,10 @@ for f in */*.tex; do
   echo -e "\n\n\\\\end{document}" >> "$tmpfile"
   mv "$tmpfile" "$f"
 done
+
+
+
+
+
+##  sed -i "s?\\\\input{Ex?%% SKIPPED %% \\\\input{Ex?" */*.tex
+##  sed -i "s/PNG/png/" */*.tex
